eda8beb798fd17fbf6da4147be78418cd4c4a7c2
David Portilla <dportilla@lean-tech.io>
2021-03-22 17:49:50 -0500

    adding expenses items

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,46 @@
 "use strict";
-console.log("Hola mundo");
+var bAdd = document.querySelector("#bAdd");
+var inputTitle = document.querySelector("#title");
+var inputCost = document.querySelector("#cost");
+var inputCurrency = document.querySelector("#currency");
+var expenses = new Expenses("USD");
+bAdd === null || bAdd === void 0 ? void 0 : bAdd.addEventListener("click", function (e) {
+    if (inputTitle.value !== "" &&
+        inputCost.value !== "" &&
+        !isNaN(parseFloat(inputCost.value))) {
+        var title = inputTitle.value;
+        var cost = parseFloat(inputCost.value);
+        var currency = inputCurrency.value;
+        expenses.add({
+            title: title,
+            cost: { number: cost, currency: currency },
+        });
+        render();
+    }
+    else {
+        alert("All data is needed");
+    }
+});
+function render() {
+    var html = "";
+    expenses.getItems().forEach(function (item) {
+        var id = item.id, title = item.title, cost = item.cost;
+        var number = cost.number, currency = cost.currency;
+        html += "\n    <div class=\"item\">\n      <div>\n        <span class=\"currency\">" + currency + "</span>" + number + "\n      </div>\n      <div>" + title + "</div>\n      <div>\n        <button class=\"bRemove\" data-id=\"" + id + "\">Remove</button>\n      </div>\n    </div>\n    ";
+    });
+    $("#items").innerHTML = html;
+    $("#display").textContent = expenses.getTotal();
+    $$(".bRemove").forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            var id = e.target.getAttribute("data-id");
+            expenses.remove(parseInt(id));
+            render();
+        });
+    });
+}
+function $(selector) {
+    return document.querySelector(selector);
+}
+function $$(selector) {
+    return document.querySelectorAll(selector);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,66 @@
-console.log("Hola mundo");
+const bAdd = document.querySelector("#bAdd") as HTMLButtonElement;
+const inputTitle = document.querySelector("#title") as HTMLInputElement;
+const inputCost = <HTMLInputElement>document.querySelector("#cost");
+const inputCurrency = <HTMLInputElement>document.querySelector("#currency");
+
+const expenses = new Expenses("USD");
+
+bAdd?.addEventListener("click", (e) => {
+  if (
+    inputTitle!.value !== "" &&
+    inputCost!.value !== "" &&
+    !isNaN(parseFloat(inputCost.value))
+  ) {
+    const title = inputTitle!.value;
+    const cost: number = parseFloat(inputCost!.value);
+    const currency = <Currency>inputCurrency!.value;
+
+    expenses.add({
+      title: title,
+      cost: { number: cost, currency: currency },
+    });
+
+    render();
+  } else {
+    alert("All data is needed");
+  }
+});
+
+function render() {
+  let html = "";
+
+  expenses.getItems().forEach((item) => {
+    const { id, title, cost } = item;
+    const { number, currency } = cost;
+
+    html += `
+    <div class="item">
+      <div>
+        <span class="currency">${currency}</span>${number}
+      </div>
+      <div>${title}</div>
+      <div>
+        <button class="bRemove" data-id="${id}">Remove</button>
+      </div>
+    </div>
+    `;
+  });
+
+  $("#items").innerHTML = html;
+  $("#display").textContent = expenses.getTotal();
+  $$(".bRemove").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const id = (e.target as HTMLButtonElement).getAttribute("data-id");
+      expenses.remove(parseInt(id!));
+      render();
+    });
+  });
+}
+
+function $(selector: string): HTMLElement {
+  return document.querySelector(selector) as HTMLElement;
+}
+
+function $$(selector: string): NodeListOf<HTMLElement> {
+  return document.querySelectorAll(selector) as NodeListOf<HTMLElement>;
+}
