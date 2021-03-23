1e70c7c4ac2d4c9ccf34a965077bef13291c26e7
David Portilla <david_portilla@hotmail.com>
2021-03-23 08:38:50 -0500

    adding styles

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ var inputTitle = document.querySelector("#title");
 var inputCost = document.querySelector("#cost");
 var inputCurrency = document.querySelector("#currency");
 var expenses = new Expenses("USD");
+render();
 bAdd === null || bAdd === void 0 ? void 0 : bAdd.addEventListener("click", function (e) {
     if (inputTitle.value !== "" &&
         inputCost.value !== "" &&
